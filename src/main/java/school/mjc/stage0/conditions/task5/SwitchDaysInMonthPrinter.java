791cef384bb0c059087b56ac415fc5c0979cf2c6
Dias <diassamarkanov108@gmail.com>
2023-02-22 20:36:06 +0600

    init: start module 4 conditions task 5

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -1,6 +1,23 @@
 package school.mjc.stage0.conditions.task5;
 
+import java.time.Month;
+
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month) {
+            case 1 -> System.out.println(Month.JANUARY.length(false));
+            case 2 -> System.out.println(Month.FEBRUARY.length(false));
+            case 3 -> System.out.println(Month.MARCH.length(false));
+            case 4 -> System.out.println(Month.APRIL.length(false));
+            case 5 -> System.out.println(Month.MAY.length(false));
+            case 6 -> System.out.println(Month.JUNE.length(false));
+            case 7 -> System.out.println(Month.JULY.length(false));
+            case 8 -> System.out.println(Month.AUGUST.length(false));
+            case 9 -> System.out.println(Month.SEPTEMBER.length(false));
+            case 10 -> System.out.println(Month.OCTOBER.length(false));
+            case 11 -> System.out.println(Month.NOVEMBER.length(false));
+            case 12 -> System.out.println(Month.DECEMBER.length(false));
+            default -> System.out.println("wrong number!");
+        }
     }
 }
